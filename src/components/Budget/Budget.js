62f1ced4fe9587cade237c83c7961f77fc7fb08f
Chad Basinger <chad.basingeris@gmail.com>
2021-03-24 00:21:53 -0600

    done

--- src/components/Budget/Budget.js
+++ src/components/Budget/Budget.js
@@ -9,7 +9,8 @@ import Nav from './../shared/Nav';
 import './Budget.css';
 import {connect} from 'react-redux'
 import {requestUserData} from '../../dux/userReducer'
-import {requestBudgetData} from '../../dux/budgetReducer'
+import {requestBudgetData, addPurchase, removePurchase} from '../../dux/budgetReducer'
+
 
 
 class Budget extends Component {
@@ -29,8 +30,8 @@ class Budget extends Component {
           <Nav firstName={firstName} lastName={lastName}/>
           <div className='content-container'>
             <div className="purchases-container">
-              <AddPurchase />
-              <DisplayPurchases purchases={purchases}/>
+              <AddPurchase addPurchase={this.props.addPurchase}/>
+              <DisplayPurchases purchases={purchases} removePurchase={this.props.removePurchase}/>
             </div>
             <div className='chart-container'>
               <Chart1 purchases={purchases} budgetLimit={budgetLimit}/>
@@ -50,4 +51,4 @@ const mapStateToProps = state => {
   }
 }
 
-export default connect(mapStateToProps, {requestUserData, requestBudgetData})(Budget);
+export default connect(mapStateToProps, {requestUserData, requestBudgetData, addPurchase, removePurchase})(Budget);
--- src/dux/budgetReducer.js
+++ src/dux/budgetReducer.js
@@ -7,12 +7,31 @@ const initialState = {
 }
 
 const REQUEST_BUDGET_DATA = 'REQUEST_BUDGET_DATA';
+const ADD_PURCHASE = 'ADD_PURCHASE';
+const REMOVE_PURCHASE = 'REMOVE_PURCHASE'
 
 export const requestBudgetData = () => {
     let responseData = axios.get('/api/budget-data').then(res => res.data)
     return {
         type: REQUEST_BUDGET_DATA,
         payload: responseData
+    }
+}
+
+export const addPurchase = (price, description, category) => {
+    let resp = axios.post('/api/budget-data/purchase', {description, price, category})
+    .then(res => res.data)
+    return {
+        type: ADD_PURCHASE,
+        payload: resp
+    }
+}
+
+export const removePurchase = (id) => {
+    let respo = axios.delete(`/api/budget-data/purchase/${id}`).then(res => res.data);
+    return{
+        type: REMOVE_PURCHASE,
+        payload: respo
     }
 }
 
@@ -29,7 +48,28 @@ export default function budgetReducer(state = initialState, action){
                 ...action.payload,
                 loading: false
             }
-
+        case ADD_PURCHASE + '_PENDING':
+            return {
+                ...state,
+                loading: true
+            }
+        case ADD_PURCHASE + '_FULFILLED':
+            return {
+                ...state,
+                purchases: action.payload,
+                loading: false
+            }
+        case REMOVE_PURCHASE + '_PENDING':
+            return{
+                ...state,
+                loading: true
+            }
+        case REMOVE_PURCHASE + '_FULFILLED':
+            return{
+                ...state,
+                loading: false,
+                purchases: action.payload
+            }
 
         default: return state;
     }
